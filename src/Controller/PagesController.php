57c1fa43bba69e61f8f292e96d89a19397377efb
AlainNgandjong <a.ngandjong@gmail.com>
2022-02-04 11:22:08 +0100

    Add logic to generate password

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -18,8 +18,37 @@ class PagesController extends AbstractController
     #[Route('/generate-password', name: 'app_generate_password')]
     public function generatePassword(Request $request): Response
     {
-        dump($request->query->all());
+        $length = $request->query->getInt('length');
+        $uppercaseLetters = $request->query->getBoolean('uppercase_letters');
+        $digits = $request->query->getBoolean('digits');
+        $specialCharacters = $request->query->getBoolean('special_characters');
 
-        return $this->render('pages/password.html.twig');
+        $characters = range('a', 'z');
+
+        if($uppercaseLetters){
+            $characters = array_merge($characters , range('A', 'Z'));
+        }
+        if($digits){
+            $characters = array_merge($characters , range(0, 9));
+        }
+        if($specialCharacters){
+            $characters = array_merge(
+                $characters ,
+                ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/',
+                     '=', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~']
+            );
+        }
+
+//        shuffle($characters); // mix the array list if needed
+        $password = '';
+
+        for ($i=0; $i < $length; $i++){
+            $password = $password.$characters[mt_rand(0, count($characters) - 1)];
+//            $password = $password.array_rand($characters, $length);
+        }
+
+        return $this->render('pages/password.html.twig', [
+            'password' => $password
+        ]);
     }
 }
