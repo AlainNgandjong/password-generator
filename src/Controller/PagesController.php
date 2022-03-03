7a898cdfe1534b7057a7e0859f450315689028e2
AlainNgandjong <a.ngandjong@gmail>
2022-03-03 10:20:56 +0100

    Prepopulate form using cookies values

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Service\PasswordGenerator;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,10 +22,13 @@ class PagesController extends AbstractController
     }
 
     #[Route('/', name: 'app_home')]
-    public function home(): Response
+    public function home(Request $request): Response
     {
         return $this->render('pages/home.html.twig', [
-            'password_default_length' => $this->parameterBag->get('app.password_default_length'),
+            'password_default_length' => $request->cookies->getInt('app_length', $this->parameterBag->get('app.password_default_length')),
+            'uppercase_letters' => $request->cookies->getBoolean('app_uppercase_letters'),
+            'digits' => $request->cookies->getBoolean('app_digits'),
+            'special_characters' => $request->cookies->getBoolean('app_special_characters'),
             'password_min_length' => $this->parameterBag->get('app.password_min_length'),
             'password_max_length' => $this->parameterBag->get('app.password_max_length'),
         ]);
@@ -53,7 +58,26 @@ class PagesController extends AbstractController
         );
 
 
-        return $this->render('pages/password.html.twig',compact('password'));
+        $response =  $this->render('pages/password.html.twig',compact('password'));
+
+        $response->headers->setCookie(
+            new  Cookie('app_length', $length, new DateTimeImmutable('+5 years'))
+        );
+
+        $response->headers->setCookie(
+            new  Cookie('app_uppercase_letters', $uppercaseLetters ? '1' : '0', new DateTimeImmutable('+5 years'))
+        );
+
+        $response->headers->setCookie(
+            new  Cookie('app_digits', $digits ? '1' : '0', new DateTimeImmutable('+5 years'))
+        );
+
+        $response->headers->setCookie(
+            new  Cookie('app_special_characters', $specialCharacters ? '1' : '0', new DateTimeImmutable('+5 years'))
+        );
+
+
+        return $response;
     }
 
 
